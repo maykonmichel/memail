1023bf3ce8b52aec3a4ed49aff491c48276a542d
maykonmichel <maykonmichelpalma@gmail.com>
2020-10-08 17:19:27 -0300

    feat: messages endpoint returning 100 generated fake messages

--- api/index.js
+++ api/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const faker = require('faker');
+
+const app = express();
 const server = http.createServer(app);
 const port = 3000;
-app.get('/', (req, res) => {
-  res.status(200).send(`Hello World! Our server is running at port ${port}`);
+
+const generateFakeMessage = () => ({
+  id: faker.random.number(),
+  timestamp: faker.date.past().getTime(),
+  subject: faker.lorem.sentence(),
+  detail: faker.lorem.paragraphs()
+})
+
+const generateFakeMessages = (samples) => [...Array(samples)].map(generateFakeMessage)
+
+const messages = generateFakeMessages(100);
+
+app.get('/messages', (req, res) => {
+  res.status(200).send(messages.sort((a, b) => a.timestamp - b.timestamp));
 });
+
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
