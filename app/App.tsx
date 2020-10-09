3ba693c259ddf899962af5fa1b8784b076795360
maykonmichel <maykonmichelpalma@gmail.com>
2020-10-09 08:16:23 -0300

    fix(ios): app safe area view

--- app/App.tsx
+++ app/App.tsx
@@ -1,22 +1,24 @@
 import React, {FC, memo} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import {ApolloProvider} from '@apollo/client';
 
 import api from './src/data/api';
 import Navigator from './src/navigators';
+import colors from './src/theme/colors';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: colors.black.dark,
   },
 });
 
 const App: FC = () => {
   return (
     <ApolloProvider client={api}>
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Navigator />
-      </View>
+      </SafeAreaView>
     </ApolloProvider>
   );
 };
